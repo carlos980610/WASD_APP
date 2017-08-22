7d417800090edd6bcead0813271bb1cdc6ab31cc
diegocorrea33 <diego.correa33@hotmail.es>
2017-08-22 11:26:19 -0500

    UpLoad 3 - Diego

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Splash_Screen/Splash_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Splash_Screen/Splash_Main_Screen.java
@@ -3,6 +3,7 @@ package com.example.carlosandres.wasd_app.Classes_Splash_Screen;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.carlosandres.wasd_app.Classes_Login_Screen.Login_Main_Screen;
 import com.example.carlosandres.wasd_app.Classes_Main_Screen.Main_Main_Screen;
@@ -13,7 +14,9 @@ public class Splash_Main_Screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_main_screen);
+
 
         Thread myThread = new Thread(){
 
