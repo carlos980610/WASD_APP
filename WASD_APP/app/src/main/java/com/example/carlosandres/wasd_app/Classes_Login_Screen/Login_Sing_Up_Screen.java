48c0c2489778c5daf1012e159a0bc0a989d744c2
diegocorrea33 <diego.correa33@hotmail.es>
2017-08-22 18:39:45 -0500

    UpLoad 7 - Diego

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Login_Screen/Login_Sing_Up_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Login_Screen/Login_Sing_Up_Screen.java
@@ -1,8 +1,13 @@
 package com.example.carlosandres.wasd_app.Classes_Login_Screen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.carlosandres.wasd_app.Classes_Main_Screen.Main_Main_Screen;
 import com.example.carlosandres.wasd_app.R;
 
 public class Login_Sing_Up_Screen extends AppCompatActivity {
@@ -11,5 +16,22 @@ public class Login_Sing_Up_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sing_up_screen);
+
+
+        final Button btnSingUp = (Button) findViewById(R.id.btnSingUp);
+        btnSingUp.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent (getApplicationContext(), Login_Main_Screen.class);
+                    startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
 }
