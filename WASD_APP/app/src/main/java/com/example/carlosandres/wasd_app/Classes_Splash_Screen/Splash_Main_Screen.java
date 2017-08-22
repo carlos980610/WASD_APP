047667fd214374d39e861d0f6a9a4a203a407246
diegocorrea33 <diego.correa33@hotmail.es>
2017-08-22 10:44:51 -0500

    UpLoad 2 - Diego
    Splash Screen

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Splash_Screen/Splash_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Splash_Screen/Splash_Main_Screen.java
@@ -1,8 +1,11 @@
 package com.example.carlosandres.wasd_app.Classes_Splash_Screen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.carlosandres.wasd_app.Classes_Login_Screen.Login_Main_Screen;
+import com.example.carlosandres.wasd_app.Classes_Main_Screen.Main_Main_Screen;
 import com.example.carlosandres.wasd_app.R;
 
 public class Splash_Main_Screen extends AppCompatActivity {
@@ -11,5 +14,22 @@ public class Splash_Main_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_main_screen);
+
+        Thread myThread = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(), Login_Main_Screen.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        myThread.start();
     }
 }
