b932bc2e6893ea6ea02ce8cf050970876b3fd65b
carlos980610 <capianomusical@gmail.com>
2017-09-03 23:27:25 -0500

    UpLoad 41 - Carlos

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Main_Screen/Main_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Main_Screen/Main_Main_Screen.java
@@ -3,6 +3,9 @@ package com.example.carlosandres.wasd_app.Classes_Main_Screen;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,10 +16,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.carlosandres.wasd_app.Publication_Data.Publication;
+import com.example.carlosandres.wasd_app.Publication_Data.Publication_Adapter;
 import com.example.carlosandres.wasd_app.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main_Main_Screen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager IManager;
+
+    private Toolbar toolbar;
+
+
+    private static ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,29 @@ public class Main_Main_Screen extends AppCompatActivity
         setContentView(R.layout.activity_main_main_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+
+        List<Publication> items = new ArrayList<>();
+
+        items.add(new Publication(R.drawable.f, "Esto es un texto de publicacion", "45", "87"));
+        items.add(new Publication(R.drawable.l, "Esto es OTRO text de prueba para una publicacion", "80", "357"));
+
+        recycler = (RecyclerView) findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);
+
+        IManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(IManager);
+
+        adapter = new Publication_Adapter(items);
+        recycler.setAdapter(adapter);
+
+
+
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
