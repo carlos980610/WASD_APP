35884a650b005f9cc90acc2c71f072cf808bff4a
carlos980610 <capianomusical@gmail.com>
2017-09-03 21:32:30 -0500

    UpLoad 39 - Carlos

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Main_Screen/Main_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Main_Screen/Main_Main_Screen.java
@@ -2,8 +2,6 @@ package com.example.carlosandres.wasd_app.Classes_Main_Screen;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,8 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.carlosandres.wasd_app.Classes_Login_Screen.Sing_Up_Screen.Login_Sing_Up_Screen;
-import com.example.carlosandres.wasd_app.Classes_Personal_Profile_Screen.My_Personal_Profile_Screen.My_Personal_Profile_Screen;
 import com.example.carlosandres.wasd_app.Publication_Data.Publication;
 import com.example.carlosandres.wasd_app.Publication_Data.Publication_Adapter;
 import com.example.carlosandres.wasd_app.R;
