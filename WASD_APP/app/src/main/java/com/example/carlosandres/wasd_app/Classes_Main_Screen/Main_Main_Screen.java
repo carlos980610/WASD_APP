2e4edf2e0bca0d7382fb116c2ad49d6d1103dd88
carlos980610 <capianomusical@gmail.com>
2017-09-11 09:30:06 -0500

    UpLoad 45 - Carlos

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Main_Screen/Main_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Main_Screen/Main_Main_Screen.java
@@ -1,11 +1,11 @@
 package com.example.carlosandres.wasd_app.Classes_Main_Screen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -17,15 +17,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.carlosandres.wasd_app.Publication_Data.Publication;
-import com.example.carlosandres.wasd_app.Publication_Data.Publication_Adapter;
+import com.example.carlosandres.wasd_app.Classes_Upload_Publication_Screen.Upload_Publication_Main_Screen;
 import com.example.carlosandres.wasd_app.Publication_Data.View_Pager_Adapter;
 import com.example.carlosandres.wasd_app.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.R.id.tabs;
 
 public class Main_Main_Screen extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,6 +74,8 @@ public class Main_Main_Screen extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                        Intent intent = new Intent (getApplicationContext(), Upload_Publication_Main_Screen.class);
+                        startActivity(intent);
             }
         });
 
