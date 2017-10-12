3208011802193dd95d81ba4319d8431ea740b24a
carlos980610 <capianomusical@gmail.com>
2017-10-12 03:30:31 -0500

    UpLoad 63 - Carlos

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Upload_Publication_Screen/Upload_Publication_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Classes_Upload_Publication_Screen/Upload_Publication_Main_Screen.java
@@ -1,10 +1,17 @@
 package com.example.carlosandres.wasd_app.Classes_Upload_Publication_Screen;
 
+import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
@@ -16,9 +23,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.carlosandres.wasd_app.Fragment_Control_Data.Fragment_Control_Publication_Data.View_Pager_Adapter;
 import com.example.carlosandres.wasd_app.R;
+
+import java.io.File;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class Upload_Publication_Main_Screen extends AppCompatActivity {
 
@@ -28,31 +41,56 @@ public class Upload_Publication_Main_Screen extends AppCompatActivity {
     private final int PHOTO_CODE=100;
     private final int SELECT_PICTURE=200;
     private ImageView imageView;
+    private final int MY_PERMISSIONS = 100;
+
+
+    private ImageView mSetImage;
+    private Button mOptionButton;
+    private RelativeLayout mRlView;
+    private String mPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_publication_main_screen);
 
-        final CharSequence [] options = {"Gallery", "Cancel"};
-        imageView = (ImageView)findViewById(R.id.imageView4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         Button button_photo = (Button)findViewById(R.id.btnSelect_From_Gallery);
         button_photo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                final CharSequence [] options = {"Galería", "Camara", "Cancelar"};
+                imageView = (ImageView)findViewById(R.id.imageView4);
 
                 final AlertDialog.Builder builder = new AlertDialog.Builder(Upload_Publication_Main_Screen.this);
-                builder.setTitle("Choose    an option");
+                builder.setTitle("Seleccione el canal");
                 builder.setItems(options  , new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int selectOP){
-                        if(options[selectOP]=="Gallery"){
+                        if(options[selectOP]=="Galería"){
                             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                             intent.setType("image/*");
                             startActivityForResult(intent.createChooser(intent, "Select image"), SELECT_PICTURE);
-                        }else if(options[selectOP]=="Cancel"){
+                        }else if(options[selectOP]=="Cancelar"){
                             dialog.dismiss();
+                        }else if(options[selectOP]=="Camara"){
+                            openCamera();
                         }
                     }
                 });
@@ -62,9 +100,54 @@ public class Upload_Publication_Main_Screen extends AppCompatActivity {
 
     }
 
+    private boolean mayRequestStoragePermission() {
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true;
+
+        if((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED))
+            return true;
+
+        if((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) || (shouldShowRequestPermissionRationale(CAMERA))){
+            Snackbar.make(mRlView, "Los permisos son necesarios para poder usar la aplicación",
+                    Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok, new View.OnClickListener() {
+                @TargetApi(Build.VERSION_CODES.M)
+                @Override
+                public void onClick(View v) {
+                    requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MY_PERMISSIONS);
+                }
+            });
+        }else{
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA}, MY_PERMISSIONS);
+        }
+        return false;
+    }
 
 
 
+    private void openCamera() {
+        mayRequestStoragePermission();
+        File file = new File(Environment.getExternalStorageDirectory(), MEDIA_DIRECTORY);
+        boolean isDirectoryCreated = file.exists();
+
+        if(!isDirectoryCreated)
+            isDirectoryCreated = file.mkdirs();
+
+        if(isDirectoryCreated){
+            Long timestamp = System.currentTimeMillis() / 1000;
+            String imageName = timestamp.toString() + ".jpg";
+
+            mPath = Environment.getExternalStorageDirectory() + File.separator + MEDIA_DIRECTORY
+                    + File.separator + imageName;
+
+            File newFile = new File(mPath);
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
+            startActivityForResult(intent, PHOTO_CODE);
+        }
+    }
 
 
 
@@ -82,12 +165,22 @@ public class Upload_Publication_Main_Screen extends AppCompatActivity {
                     Uri path = data.getData();
                     imageView.setImageURI(path);
                     break;
+                case PHOTO_CODE:
+                    MediaScannerConnection.scanFile(this,
+                            new String[]{mPath}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                @Override
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("ExternalStorage", "Scanned " + path + ":");
+                                    Log.i("ExternalStorage", "-> Uri = " + uri);
+                                }
+                            });
+
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(mPath);
+                    mSetImage.setImageBitmap(bitmap);
+                    break;
             }
         }
     }
-
-
-
-
-
 }
