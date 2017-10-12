83b41c335311fba739de6433ad998c4e79f9b244
diegocorrea33 <diego.correa33@hotmail.es>
2017-10-12 08:26:12 -0500

    UpLoad 30 - Diego

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Fragment_Control_Data/Fragment_Control_Publication_Data/First_Fragment_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Fragment_Control_Data/Fragment_Control_Publication_Data/First_Fragment_Main_Screen.java
@@ -36,6 +36,14 @@ public class First_Fragment_Main_Screen extends Fragment {
 
         items.add(new Publication_Main_Screen(R.drawable.f, "Esto es un texto de publicacion", "45", "87", "Iva025", "Envigado", R.drawable.i));
         items.add(new Publication_Main_Screen(R.drawable.l, "Esto es OTRO text de prueba para una publicacion", "80", "357", "CAMC", "IUE", R.drawable.u));
+        items.add(new Publication_Main_Screen(R.drawable.q, "Esto es un texto de publicacion", "78", "504", "Granaada", "Sabaneta", R.drawable.i));
+        items.add(new Publication_Main_Screen(R.drawable.w, "Esto es OTRO text de prueba para una publicacion", "140", "34", "Karrim058", "Poblado", R.drawable.u));
+        items.add(new Publication_Main_Screen(R.drawable.e, "Esto es un texto de publicacion", "95", "147", "ConNvidia4", "En la Grieta", R.drawable.i));
+        items.add(new Publication_Main_Screen(R.drawable.r, "Esto es OTRO text de prueba para una publicacion", "870", "3757", "Larrys69", "En too lado", R.drawable.u));
+        items.add(new Publication_Main_Screen(R.drawable.t, "Esto es un texto de publicacion", "35", "78", "TheJimmyNeut74", "Itagui", R.drawable.i));
+        items.add(new Publication_Main_Screen(R.drawable.a, "Esto es OTRO text de prueba para una publicacion", "860", "3507", "IoCiBi", "San Jose", R.drawable.u));
+        items.add(new Publication_Main_Screen(R.drawable.z, "Esto es un texto de publicacion", "489", "787", "TheChapther1", "Envigado", R.drawable.i));
+        items.add(new Publication_Main_Screen(R.drawable.x, "Esto es OTRO text de prueba para una publicacion", "880", "457", "CAMC", "IUE", R.drawable.u));
 
 
         final Publication_Adapter_Main_Screen itemsAdapter = new Publication_Adapter_Main_Screen(items);
