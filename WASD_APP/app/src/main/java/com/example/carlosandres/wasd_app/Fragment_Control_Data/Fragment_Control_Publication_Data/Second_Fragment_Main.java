5084986eb688a50e775be0c85d4f5289b3d0da50
diegocorrea33 <diego.correa33@hotmail.es>
2017-11-21 20:35:41 -0500

    UpLoad 36 - Diego

--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Fragment_Control_Data/Fragment_Control_Publication_Data/Second_Fragment_Main.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Fragment_Control_Data/Fragment_Control_Publication_Data/Second_Fragment_Main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.carlosandres.wasd_app.DataBase.Entities.User_Entity;
+import com.example.carlosandres.wasd_app.DataBase.Store_Procedures.Login_Store_Procedures.SP_Get_User_Data;
 import com.example.carlosandres.wasd_app.R;
 
 import java.util.ArrayList;
@@ -39,6 +42,22 @@ public class Second_Fragment_Main extends Fragment {
     public void onViewCreated(View v, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(v, savedInstanceState);
 
+        SP_Get_User_Data SP_Get_User_Data = new SP_Get_User_Data(this.getContext());
+
+        List<User_Main_Screen> items = new ArrayList<User_Main_Screen>();
+/*
+        items.add(new User_Main_Screen("Carlos", "Andres", "MMontoya", "18", "capiano@C"));
+        items.add(new User_Main_Screen("Juan", "Jose", "CCarvajal", "18", "juanj123@C"));
+        items.add(new User_Main_Screen("Diego", "Alejandro", "CCorrea", "19", "die.c33@C"));
+        items.add(new User_Main_Screen("Rel", "leno", "AlgunaCosa", "20", "any@C"));
+*/
+        items = SP_Get_User_Data.Get_User_Data();
+
+        final User_Adapter_Main_Screen itemsAdapter = new User_Adapter_Main_Screen(items);
+        final RecyclerView clv = (RecyclerView) v.findViewById(R.id.recycler_users);
+        clv.setLayoutManager(new LinearLayoutManager(Second_Fragment_Main.this.getActivity()));
+        clv.setHasFixedSize(true);
+        clv.setAdapter(itemsAdapter);
         /*ArrayList<User_Entity> User = new ArrayList<User_Entity>();
         lv = (ListView) lv.findViewById(R.id.UserView);
 
--- WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Fragment_Control_Data/Fragment_Control_Publication_Data/User_Adapter_Main_Screen.java
+++ WASD_APP/app/src/main/java/com/example/carlosandres/wasd_app/Fragment_Control_Data/Fragment_Control_Publication_Data/User_Adapter_Main_Screen.java
@@ -6,6 +6,7 @@ package com.example.carlosandres.wasd_app.Fragment_Control_Data.Fragment_Control
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,68 +14,63 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.List;
+
 import android.widget.BaseAdapter;
 
 import com.example.carlosandres.wasd_app.DataBase.Entities.User_Entity;
 import com.example.carlosandres.wasd_app.R;
 
-public class User_Adapter_Main_Screen extends BaseAdapter{
+public class User_Adapter_Main_Screen extends RecyclerView.Adapter<User_Adapter_Main_Screen.UserViewHolder>{
 
-    protected Activity activity;
-    protected ArrayList<User_Entity> items;
+    private List<User_Main_Screen> items;
 
-    public User_Adapter_Main_Screen (Activity activity, ArrayList<User_Entity> items) {
-        this.activity = activity;
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView NickName_User;
+        public TextView Name_User;
+        public TextView Last_Name_User;
+        public TextView Age_User;
+        public TextView Email_User;
+        public ImageView Profile_User_Img;
+
+
+        public UserViewHolder (View v){
+            super(v);
+
+            Profile_User_Img = (ImageView) v.findViewById(R.id.imageUser);
+            Name_User = (TextView) v.findViewById(R.id.UserName);
+            Last_Name_User = (TextView) v.findViewById(R.id.LastUserName);
+            Age_User = (TextView) v.findViewById(R.id.ageUser);
+            Email_User = (TextView) v.findViewById(R.id.EmailUser);
+            NickName_User = (TextView) v.findViewById(R.id.NickName);
+        }
+
+    }
+
+    public User_Adapter_Main_Screen(List<User_Main_Screen> items) {
         this.items = items;
     }
 
-    @Override
-    public int getCount() {
+    @Override //Ok
+    public int getItemCount(){
         return items.size();
     }
 
-    public void clear() {
-        items.clear();
+    @Override //Ok
+    public User_Adapter_Main_Screen.UserViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_card, viewGroup, false);
+        return new UserViewHolder(v);
     }
+    @Override //Ok
+    public void onBindViewHolder(UserViewHolder viewHolder, int i){
 
-    public void addAll(ArrayList<User_Entity> category) {
-        for (int i = 0; i < category.size(); i++) {
-            items.add(category.get(i));
-        }
+        viewHolder.Name_User.setText(items.get(i).getName_User());
+        viewHolder.NickName_User.setText(items.get(i).getNickName_User());
+        viewHolder.Last_Name_User.setText(items.get(i).getLast_Name_User());
+        viewHolder.Age_User.setText(items.get(i).getAge_User());
+        viewHolder.Email_User.setText(items.get(i).getEmail_User());
+
     }
-
-    @Override
-    public Object getItem(int arg0) {
-        return items.get(arg0);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        View v = convertView;
-
-        if (convertView == null) {
-            LayoutInflater inf = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = inf.inflate(R.layout.user_card, null);
-        }
-
-        User_Entity UD = items.get(position);
-
-        TextView UserName = (TextView) v.findViewById(R.id.UserName);
-        UserName.setText(UD.Name_User);
-
-        TextView NickName = (TextView) v.findViewById(R.id.NickName);
-        NickName.setText(UD.NickName_User);
-
-        //ImageView imagen = (ImageView) v.findViewById(R.id.imageView);
-        //imagen.setImageDrawable(UD.);
-
-        return v;
-    }
-
 }
